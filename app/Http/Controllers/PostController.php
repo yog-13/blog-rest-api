c11342e30aee8bd52e6a04f4b8f6dfb0a90e0ccd
yogapratama13 <ekapratama1913@gmail.com>
2024-05-16 15:02:35 +0700

    menambahkan logic api untuk delete data post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -96,8 +96,12 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        return response()->json([
+            'message' => 'Success Delete Post Data',
+        ], 200);
     }
 }
