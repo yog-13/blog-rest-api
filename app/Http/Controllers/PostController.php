0c987a4b66f8ceac3c5a8d0df8d0fedcbc877c83
yogapratama13 <ekapratama1913@gmail.com>
2024-05-20 22:45:37 +0700

    menghapus function create() & edit()

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -32,16 +32,6 @@ class PostController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -91,17 +81,6 @@ class PostController extends Controller
         return response()->json([
             'post' => new PostResource($post),
         ], 200);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
